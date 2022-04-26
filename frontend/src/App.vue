c26666d277c7614109ac95a4843c1f5c3a315fe8
zen2kx <pazemaze@gmail.com>
2022-04-26 22:52:46 +0200

    +setup new vue.js project

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap.scss";
+$fa-font-path: "~@fortawesome/fontawesome-free/webfonts";
+@import "~@fortawesome/fontawesome-free/scss/fontawesome";
+@import "~@fortawesome/fontawesome-free/scss/solid"; // fas
+@import "~@fortawesome/fontawesome-free/scss/regular"; // far
+@import "~@fortawesome/fontawesome-free/scss/brands"; // fab
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach(type =>
+      addStyleResource(config.module.rule("scss").oneOf(type))
+    );
+  }
+})
+
+/* SCSS support (https://vinceumo.github.io/devNotes/Javascript/vue-scss-setup/) */
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [
+        path.resolve(__dirname, "./src/styles/functions/*.scss"),
+        path.resolve(__dirname, "./src/styles/mixins/*.scss"),
+        path.resolve(__dirname, "./src/styles/variables/*.scss")
+      ]
+    });
+}

38ae3a72efabf6f1e6a42b1945aff107ce1aea57
valar <68338647+76616c6172@users.noreply.github.com>
2022-05-08 16:46:40 -0700

    Merged frontend changes and added push_frontend_to_server

--- website/controller/main.go
+++ website/controller/main.go
@@ -4,22 +4,9 @@ import (
 	"exia/controller/internals"
 
 	"net/http"
-	"text/template"
 )
 
 const WEBSERVER_PORT = ":8080"
-
-var view *template.Template
-
-// Know which templates to use
-func init() {
-	view = template.Must(template.ParseGlob("../view/*.gohtml"))
-}
-
-// Handler for all requests to www.the_url/
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	view.ExecuteTemplate(w, "index.gohtml", nil)
-}
 
 // Answers calls to the endpoint /api/0/jobs
 func api_0_jobs(w http.ResponseWriter, r *http.Request) {
@@ -39,12 +26,6 @@ func api_0_jobs(w http.ResponseWriter, r *http.Request) {
 // TODO: requires a jobid and sends back the latest image for that job id.
 func api_0_img(w http.ResponseWriter, r *http.Request) {
 	internals.HandleImgRequests(w, r) // TODO: Return the correct image based on the request (request with jobid)
-
-	/*
-		if r.Method == "POST" {
-		}
-	*/
-
 }
 
 // Answers calls to the endpoint /api/0/all
@@ -56,9 +37,8 @@ func api_0_status(w http.ResponseWriter, r *http.Request) {
 // This is the main function :D
 func main() {
 
-	// Handle requests for web assets
-	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("../view/dist")))) //serves requests to www.url/assets/
-	http.HandleFunc("/", index_handler)                                                             // handles requests to /
+	// Handle requests for assets, everything in ../view/dist is accessible to the public
+	http.Handle("/", http.FileServer(http.Dir("../view/dist"))) //serves requests to www.url/assets/
 
 	// Handle API endpoints
 	http.HandleFunc("/api/0/status", api_0_status) // registers api_0_status() as the handler for "/api/0/status"

7ae81e7ea158ef404a2c4c02e712dac0a59f4703
zen2kx <pazemaze@gmail.com>
2022-05-15 20:10:24 +0200

    spinner

--- website/view/src/components/content/ImageComponent.vue
+++ website/view/src/components/content/ImageComponent.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <div class="mb-5 mt-5">
-      <div v-if="isLoading" class="spinner-border text-secondary" role="status">
+      <!-- <div v-if="isLoading" class="spinner-border text-secondary" role="status">
         <span class="visually-hidden">Loading...</span>
-      </div>
+      </div> -->
+      <div v-if="isLoading" class="loader">Loading...</div>
       <img :src="imgObjectURL" class="img-fluid img-thumbnail" alt="" />
     </div>
   </div>
@@ -51,4 +52,62 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.loader,
+.loader:before,
+.loader:after {
+  border-radius: 50%;
+  width: 2.5em;
+  height: 2.5em;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation: load7 1.8s infinite ease-in-out;
+  animation: load7 1.8s infinite ease-in-out;
+}
+.loader {
+  color: #2ec27e;
+  font-size: 10px;
+  margin: 80px auto;
+  position: relative;
+  text-indent: -9999em;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  content: "";
+  position: absolute;
+  top: 0;
+}
+.loader:before {
+  left: -3.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 3.5em;
+}
+@-webkit-keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+@keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+</style>

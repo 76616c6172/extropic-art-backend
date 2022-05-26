c4102a3effc0c0d846d83d470524eaeda132c9be
zen2kx <pazemaze@gmail.com>
2022-05-26 15:49:43 +0200

    5 items in select

--- website/view/src/store/modules/api/index.js
+++ website/view/src/store/modules/api/index.js
@@ -7,7 +7,7 @@ const state = {
   selectedJob: [],
   jobRange: {
     jobx: 1,
-    joby: 10,
+    joby: 5,
   },
 };
 const getters = {
@@ -26,16 +26,16 @@ const actions = {
   setJobRange(_, scrollDirection) {
     switch (scrollDirection) {
       case "down":
-        state.jobRange.jobx += 1;
-        state.jobRange.joby += 10;
+        state.jobRange.jobx = state.jobRange.joby + 1;
+        state.jobRange.joby += 5;
         break;
       case "up":
         if (state.jobRange.jobx == 1) {
           state.jobRange.jobx = 1;
-          state.jobRange.joby = 10;
+          state.jobRange.joby = 5;
         } else {
-          state.jobRange.jobx -= 1;
-          state.jobRange.joby -= 10;
+          state.jobRange.jobx = state.jobRange.joby;
+          state.jobRange.joby -= 5;
         }
         break;
     }

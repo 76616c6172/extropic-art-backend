fd257fe6e5f0bcd34c51a7cf5f71a91142529f59
zen2kx <pazemaze@gmail.com>
2022-06-06 18:52:30 +0200

    deleted emitters, added watcher HomeComp

--- website/view/src/components/content/TerminalWrapperComponent.vue
+++ website/view/src/components/content/TerminalWrapperComponent.vue
@@ -8,7 +8,7 @@
         <div class="ui-circle ui-circle-green rounded-pill"></div>
       </div>
       <ItemList />
-      <Prompt @set-newJob="emitNewJob()" :showCursor="showCursor" />
+      <Prompt :showCursor="showCursor" />
     </div>
   </div>
 </template>
@@ -26,11 +26,6 @@ export default {
     showCursor: {
       type: Boolean,
       required: true,
-    },
-  },
-  methods: {
-    emitNewJob() {
-      this.$emit("set-newJob");
     },
   },
 };

2ad1753ec47ee0dc663c3410ac5eb8afdbb981ce
valar <68338647+76616c6172@users.noreply.github.com>
2022-07-10 20:56:37 -0700

    VIEW: Visual changes - reduced footer size and changed text color

--- src/view/src/components/general/FooterComponent.vue
+++ src/view/src/components/general/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="footer text-muted mt-auto py-3 bg-light">
-    <div class="text-center p-3">
+  <footer class="footer text-default mt-auto py-3 bg-light">
+    <div class="text-center p-10">
       Made with <i class="fa fa-heart" aria-hidden="true"></i> by valar and
       zen in {{ getYear }}
     </div>

b87e988b4d874d9c4b9a87c6f11a6b1c0c8ae239
valar <68338647+76616c6172@users.noreply.github.com>
2022-11-03 12:49:50 -0700

    feat(flake.nix): add nix flake to make dev environment portable

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+	description = "defines extropic-art-backend dev environment in a nix flake";
+	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	# TODO insert tests
+	outputs = { self, nixpkgs }:
+		let
+      version = "build-${builtins.substring 0 8 self.lastModifiedDate}";
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+		in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+						packages = [
+							pkgs.go 
+						];
+          shellHook = 
+          ''
+            unset GOPATH
+            unset GOROOT
+          '';
+          };
+        });
+    };
+}
